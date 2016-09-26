389e0437d0cd50fd312b1a9c6259909a4fa36735
Victor Eremin <altctrlsysrqb@gmail.com>
2016-09-26 14:09:13 +0300

    process component functions

--- Assets/Scripts/Extensions/ObjectExtensions.cs
+++ Assets/Scripts/Extensions/ObjectExtensions.cs
@@ -8,6 +8,11 @@ namespace ObjectExtensions{
 				throw new System.ArgumentNullException();
 		}
 
+		static void nullCheck(Component component){
+			if (!component)
+				throw new System.ArgumentNullException();
+		}
+
 		public static Comp getOrCreateComponent<Comp>(this GameObject gameObject, System.Action<Comp> initializer = null) where Comp: Component{
 			nullCheck(gameObject);
 			var result = gameObject.GetComponent<Comp>();
@@ -17,6 +22,11 @@ namespace ObjectExtensions{
 					initializer(result);
 			}
 			return result;
+		}
+
+		public static Comp getOrCreateComponent<Comp>(this Component curComponent, System.Action<Comp> initializer = null) where Comp: Component{
+			nullCheck(curComponent);
+			return curComponent.gameObject.getOrCreateComponent<Comp>(initializer);
 		}
 
 		public static bool processComponent<Comp>(this GameObject gameObject, System.Action<Comp> action) where Comp: Component{
@@ -30,9 +40,36 @@ namespace ObjectExtensions{
 			return true;
 		}
 
+		public static bool processComponent<Comp>(this Component curComponent, System.Action<Comp> action) where Comp: Component{
+			nullCheck(curComponent);
+			return curComponent.gameObject.processComponent(action);
+		}
+
+		public static bool processComponent<Comp, Data>(this GameObject gameObject, System.Action<Comp, Data> action, Data data) where Comp: Component{
+			nullCheck(gameObject);
+			var comp = gameObject.GetComponent<Comp>();
+			if (!comp)
+				return false;
+			if (action != null){
+				action(comp, data);
+			}
+			return true;
+		}
+
+		public static bool processComponent<Comp, Data>(this Component curComponent, System.Action<Comp, Data> action, Data data) where Comp: Component{
+			nullCheck(curComponent);
+			return curComponent.gameObject.processComponent<Comp, Data>(action, data);
+		}
+
+
 		public static bool hasComponent<Comp>(this GameObject gameObject) where Comp: Component{
 			nullCheck(gameObject);
 			return gameObject.GetComponent<Comp>();
+		}
+
+		public static bool hasComponent<Comp>(this Component curComponent) where Comp: Component{
+			nullCheck(curComponent);
+			return curComponent.gameObject.hasComponent<Comp>();
 		}
 
 		public static void setLocalTransform(this GameObject gameObject, Vector3 position, Quaternion rotation, Vector3 scale){
@@ -92,6 +129,92 @@ namespace ObjectExtensions{
 		public static void setParent(this GameObject gameObject, Transform newParent, bool worldPositionStays = true){
 			nullCheck(gameObject);
 			gameObject.transform.SetParent(newParent, worldPositionStays);
+		}
+
+		//childAction must return true in order to continue traversing hierarchy
+		public static void processHierarchy(this GameObject gameObject, System.Func<GameObject, bool> childAction){
+			if (!gameObject)
+				return;
+			if (childAction == null)
+				return;
+			if (!childAction(gameObject))
+				return;
+			foreach(Transform cur in gameObject.transform){
+				cur.gameObject.processHierarchy(childAction);
+			}
+		}
+
+		//childAction must return true in order to continue traversing hierarchy
+		public static void processHierarchy<Data>(this GameObject gameObject, System.Func<GameObject, Data, bool> childAction, Data data){
+			if (!gameObject)
+				return;
+			if (childAction == null)
+				return;
+			if (!childAction(gameObject, data))
+				return;
+			foreach(Transform cur in gameObject.transform){
+				cur.gameObject.processHierarchy(childAction, data);
+			}
+		}
+
+		struct ProcessComponentsPayload<Comp> where Comp: Component{
+			public System.Action<Comp> componentAction;
+			public System.Func<GameObject, bool> objectFilter;
+		}
+
+		struct ProcessComponentsDataPayload<Comp, Data> where Comp: Component{
+			public System.Action<Comp, Data> componentAction;
+			public System.Func<GameObject, Data, bool> objectFilter;
+			public Data data;
+		}
+
+		public static void processComponentsInChildren<Comp>(this GameObject gameObject, 
+				System.Action<Comp> componentAction, System.Func<GameObject, bool> objectFilter) where Comp: Component{
+			if (componentAction == null)
+				return;
+			if (gameObject && (objectFilter != null) && !objectFilter(gameObject))
+				return;
+			var payload = new ProcessComponentsPayload<Comp>();
+			payload.componentAction = componentAction;
+			payload.objectFilter = objectFilter;
+			gameObject.processHierarchy((obj, data) => {
+				if ((data.objectFilter != null) && (!data.objectFilter(obj)))
+					return false;
+				obj.processComponent<Comp>(data.componentAction);
+				return true;
+			}, payload);			
+		}
+
+		public static void processComponentsInChildren<Comp>(this Component curComponent, 
+				System.Action<Comp> componentAction, System.Func<GameObject, bool> objectFilter) where Comp: Component{
+			if (!curComponent)
+				return;
+			curComponent.gameObject.processComponentsInChildren<Comp>(componentAction, objectFilter);
+		}
+
+		public static void processComponentsInChildren<Comp, Data>(this GameObject gameObject, 
+				System.Action<Comp, Data> componentAction, System.Func<GameObject, Data, bool> objectFilter, Data data) where Comp: Component{
+			if (componentAction == null)
+				return;
+			if (gameObject && (objectFilter != null) && !objectFilter(gameObject, data))
+				return;
+			var payload = new ProcessComponentsDataPayload<Comp, Data>();
+			payload.componentAction = componentAction;
+			payload.objectFilter = objectFilter;
+			payload.data = data;
+			gameObject.processHierarchy((_obj, _objData) => {
+				if ((_objData.objectFilter != null) && (!_objData.objectFilter(_obj, _objData.data)))
+					return false;
+				_obj.processComponent<Comp, Data>(_objData.componentAction, _objData.data);
+				return true;
+			}, payload);			
+		}
+
+		public static void processComponentsInChildren<Comp, Data>(this Component curComponent, 
+				System.Action<Comp, Data> componentAction, System.Func<GameObject, Data, bool> objectFilter, Data data) where Comp: Component{
+			if (!curComponent)
+				return;
+			curComponent.gameObject.processComponentsInChildren<Comp, Data>(componentAction, objectFilter, data);
 		}
 	}		
 }
